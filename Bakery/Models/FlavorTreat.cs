8bc8ff3a70d12566fd43c2b3f76a52439c1ddac8
Vera Weikel <vera.weikel@gmail.com>
2023-03-24 09:50:12 -0700

    Add Models/App User, Context, and join table.

--- Bakery/Models/FlavorTreat.cs
+++ Bakery/Models/FlavorTreat.cs
@@ -1,4 +1,11 @@
 using Bakery.Models
 {
   public class FlavorTreat
+  {
+    public int FlavorTreatId { get; set; }
+    public int FlavorId { get; set; }
+    public int TreatId { get; set; } 
+    public Flavor Flavor { get; set; } 
+    public Treat Treat { get; set; } 
+  }
 }
